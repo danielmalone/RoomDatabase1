a59617bceaabc1d07e9dea126e3cb68feeebde5f
Daniel Malone <danieljmalone@gmail.com>
2017-05-19 17:51:02 -0500

    add User class, annotations

--- app/src/main/java/com/finepointstudios/roomdatabase1/User.java
+++ app/src/main/java/com/finepointstudios/roomdatabase1/User.java
@@ -1,8 +1,34 @@
 package com.finepointstudios.roomdatabase1;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
 /**
  * Created by danielmalone on 5/19/17.
  */
 
+@Entity
 public class User {
+    @PrimaryKey(autoGenerate = true)
+    private int uid;
+
+    @ColumnInfo(name = "first_name")
+    private String firstName;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 }
